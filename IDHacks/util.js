8bb9a608b511a83b46a1de1e79af4aaabde7782b
Gurneet <gurneet.k.arora@gmail.com>
2014-02-16 04:50:20 -0500

    hi

--- IDHacks/util.js
+++ IDHacks/util.js
@@ -1,7 +1,7 @@
 $(document).on("pageinit", "#diagnosis",function(){
 	$.getJSON("localhost:5000/diagnosis", function(data){
 		console.log(data);	
-	}
+	});
 });
 
 $(document).on("pageinit", "#prescription", function(){
