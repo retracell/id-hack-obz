8e26f111ca954b8a6758162c7b01d564e5b0d5f2
Gurneet <gurneet.k.arora@gmail.com>
2014-02-16 02:53:14 -0500

    updates

--- IDHacks/util.js
+++ IDHacks/util.js
@@ -1,11 +1,12 @@
 $(document).on("pageinit", "#home",function(){
-$.get("symptoms.txt", function(data){
-	console.log("here");
-/*
-	symp = data.split("\n");
-	len = symp.length;
-	for (i=0; i<len; i++){
-		$("ul").append("<li><a href='#'>"+symp[0]+"</a></li>");
-	}*/
-});
+
+	$.get("/symptoms.txt", function(data){
+		console.log("here");
+	
+		symp = data.split("\n");
+		len = symp.length;
+		for (i=0; i<len; i++){
+			$("ul").append("<li><a href='#'>"+symp[0]+"</a></li>");
+		}
+	});
 });
