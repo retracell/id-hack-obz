dfb6a4c757f86cd5eaa3f3eb962912d9d437703d
Ryan Zhang <xy22zhan@uwaterloo.ca>
2014-02-16 11:50:23 -0500

    Frontend for prescription page

--- IDHacks/main.js
+++ IDHacks/main.js
@@ -4,15 +4,26 @@ $(document).on('pagebeforeshow', '#index', function(){
         $.mobile.changePage("#second");
     });
 
+});
+
+$(document).on('pagebeforeshow', '#index', function(){
     $(document).on('click', '#change-page-prescription', function(){
         $.mobile.changePage("#third");
     });
 });
 
-
 $(document).on("pageshow", "#second", function(){
-    
+   
     $("#second #patient-name").text(storeObject.patient);
+    for (i=0;i<storeObject.diagnosis.length; i++){
+            $("fieldset").append("<input type='radio' name='radio-choice' value="+i
+            +"/><label for='radio-choice-2'>"+storeObject.diagnosis[i][1]+"</label>"    );
+    }
+});
+
+$(document).on("pageshow", "#third", function(){
+    $("#third #patient-name").text(storeObject.patient);
+    $("#third #diagnosis-name").text(storeObject.diagnose);
     for (i=0;i<storeObject.diagnosis.length; i++){
             $("fieldset").append("<input type='radio' name='radio-choice' value="+i
             +"/><label for='radio-choice-2'>"+storeObject.diagnosis[i][1]+"</label>");
